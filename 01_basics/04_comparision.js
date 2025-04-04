787035f747e0876f273688dc1184f4abcca1c4c5
sambhav sharma <ssambhav982@gmail.com>
2025-04-04 09:35:38 +0000

    04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//we have to avoid using null and undefined words in code for making it more clean.
+// console.log(null > 0); //The Reason is that an equality check == and comparisons >,<,>=,<= work differently. 
+// console.log(null == 0);// comparision convert null to a number ,treating it as 0.
+// console.log(null >= 0);// that's why (In 13position) null is true and (In 11position) is false.
+
+console.log(undefined == 0); //if we compare "undefined" with any number it will always be False. 
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//===
+
+console.log("2" === 2);
+
+
+
+
