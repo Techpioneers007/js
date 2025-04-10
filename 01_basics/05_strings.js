e69ce6f562982bdb76022de77cb5d3f9af1cb48e
sambhav sharma <ssambhav982@gmail.com>
2025-04-10 19:17:51 +0000

    05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,5 +5,31 @@ const repoCount= 30;
 
 console.log(`${name} has ${repoCount} repositories.`); // Template literals
 
-const gameName = new String("Call of Duty");
-console.log(gameName); // String object
+const gameName = new String('Call-of-Duty')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt('8'));
+console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0,4) //in substring we cannot gives negative value,it will ignor and start from "0".
+console.log(newString);
+
+const anotherSring = gameName.slice(-8,4)
+console.log(anotherSring);
+
+const newStringone = "  sambhav   "
+console.log(newStringone);
+console.log(newStringone.trim());
+
+const url = "https://sambhav.com/sambhav77sharma"
+
+console.log (url.replace('77','_'))
+
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
